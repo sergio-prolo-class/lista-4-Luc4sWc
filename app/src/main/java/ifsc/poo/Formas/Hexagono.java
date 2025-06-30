9d60297bff0233cc3cdbac304deee163a4fba91a
Luc4sWc <164187084+Luc4sWc@users.noreply.github.com>
2025-06-30 08:52:57 -0300

    Update Hexagono.java

--- app/src/main/java/ifsc/poo/Formas/Hexagono.java
+++ app/src/main/java/ifsc/poo/Formas/Hexagono.java
@@ -6,8 +6,8 @@ import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 
 public class Hexagono extends FormaAbstrata {
-    private double []x = new double [6];
-    private double []y = new double [6];
+    private double []vert_x = new double [6];
+    private double []vert_y = new double [6];
     private final double lado;
 
     public Hexagono(double centroX, double centroY, double halflength, boolean preenchido, Color cor) {
@@ -16,33 +16,33 @@ public class Hexagono extends FormaAbstrata {
 
         for (int i = 0; i < 6; i++) {
             double angle = Math.toRadians(60 * i);
-            x[i] = centroX + lado * Math.cos(angle);
-            y[i] = centroY + lado * Math.sin(angle);
+            vert_x[i] = centroX + lado * Math.cos(angle);
+            vert_y[i] = centroY + lado * Math.sin(angle);
         }
     }
 
     private double getRaio() {
-        return Math.hypot(x[0] - x[3], y[0] - y[3]) / 2;
+        return Math.hypot(vert_x[0] - vert_x[3], vert_y[0] - vert_y[3]) / 2;
     }
 
     @Override
     public void desenhar(Draw d) {
         d.setPenColor(cor);
         if (preenchido) {
-            d.filledPolygon(this.x, this.y);
+            d.filledPolygon(this.vert_x, this.vert_y);
             d.setPenColor(Color.black);
-            d.polygon(this.x,this.y);
+            d.polygon(this.vert_x,this.vert_y);
 
         } else {
-            d.polygon(this.x, this.y);
+            d.polygon(this.vert_x, this.vert_y);
         }
     }
 
     @Override
     public void mover(double dx, double dy) {
         for (int i = 0; i < 6; i++) {
-            this.x[i] += dx;
-            this.y[i] += dy;
+            this.vert_x[i] += dx;
+            this.vert_y[i] += dy;
         }
     }
 
